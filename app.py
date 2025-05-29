ee3d27fc8e81b127cb02d5f94d658ae9d52b3c67
SompheaReak <Thesonggaming@gmail.com>
2025-05-29 23:49:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 import datetime
 def notify_telegram(ip, user_agent):
     bot_token = "7663680888:AAHhInaDKP8QNxw8l87dQaNPsRTZFQXy1J4"
-    chat_id = "-1002660809745"
+    chat_id = "-10026600809745"
     message = f"🌐 *New Visitor Alert!*\n\n*IP:* `{ip}`\n*Device:* `{user_agent}`"
 
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
